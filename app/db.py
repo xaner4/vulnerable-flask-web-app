107d9f0f4b6468657d6f735c9af81bba2ad9f90a
Marius <marius-sol@pm.me>
2020-04-23 16:23:55 +0200

    Added contex manager to database

--- app/db.py
+++ app/db.py
@@ -2,6 +2,8 @@ import os
 import sqlite3
 
 import click
+from flask import current_app, g
+from flask.cli import with_appcontext
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
 db_path = os.path.join(base_dir, "../db/")
@@ -10,20 +12,28 @@ schema = os.path.join(db_path, "schema.sql")
 
 class DB:
     def open_db():
-        db = sqlite3.connect(database)
-        return db
+        if "db" not in g:
+            g.db = sqlite3.connect(database)
+        return g.db
 
-    def close_db():
-        pass
-
+    def close_db(e=None):
+        db = g.pop("db", None)
+        if db is not None:
+            db.close()
+    
     def initialize():
         db = DB.open_db()
-        with open(schema, mode="r", encoding="utf-8") as f:
-            DB.open_db().executescript(f.read())
+        with current_app.open_resource(schema) as f:
+            db.executescript(f.read().decode("utf8"))
+
 
 
 @click.command("init-db")
+@with_appcontext
 def init_db_command():
-    """Clear existing data and create new tables."""
-    init_db()
+    DB.initialize()
     click.echo("Initialized the database.")
+
+def init_app(app):
+    app.teardown_appcontext(DB.close_db)
+    app.cli.add_command(init_db_command)
