0810b14d801ae6de050d2e0c55427355947f42c7
Marius <marius-sol@pm.me>
2020-05-01 19:28:06 +0200

    Edit posts

--- app/views/posts.py
+++ app/views/posts.py
@@ -2,14 +2,17 @@ import time
 from flask import Blueprint, redirect, render_template, request, url_for
 from werkzeug.exceptions import abort
 
-from app.db import DB as db
+from app.db import posts_db
 
 bp = Blueprint("posts", __name__)
 
 @bp.route("/<int:id>")
 def post(id: int):
-    post = db.get_post(id)
-    return render_template("post/post.html", post=post)
+    if id:
+        post = posts_db.get_post(id)
+        return render_template("post/post.html", post=post)
+    else:
+        abort(404, f"A post with id: '{post_id}' does not exsist")
 
 @bp.route("/add", methods=["GET", "POST"])
 def add_post():
@@ -21,8 +24,24 @@ def add_post():
         visebility = request.form["visebility"]
         if not header or not article:
             return render_template("post/add.html", err="Header or Article missing")
-        else
-            db.add_post(author_id, publised_at, header, article, visebility)
-            return redirect(url_for("posts.post"))
+        else:
+            posts_db.add_post(author_id, publised_at, header, article, visebility)
+            return redirect("/", err=err)
     else:
-        pass
+        return render_template("post/add.html")
+
+@bp.route("/edit/<int:post_id>", methods=["GET", "POST"])
+def edit_post(post_id: int):
+    if request.method == "POST":
+        edited_at = int(time.time())
+        header = request.form["header"]
+        article = request.form["article"]
+        visebility = request.form["visebility"]
+        posts_db.edit_post(post_id, edited_at, header, article, visebility)
+    else:
+        post = posts_db.get_post(id)
+        if post is None:
+            abort(404, f"A post with id {post_id} does not exsist")
+        else:
+            return render_template("post/edit.html", post=post)
+        
