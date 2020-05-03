2eaf3943e45d478107f0d09618c8c0d4c2e6d360
Marius <marius-sol@pm.me>
2020-05-03 22:36:32 +0200

    Changed id to post_id and fixed head to title

--- app/views/posts.py
+++ app/views/posts.py
@@ -6,26 +6,27 @@ from app.db import posts_db
 
 bp = Blueprint("posts", __name__)
 
-@bp.route("/<int:id>")
-def post(id: int):
-    if id:
-        post = posts_db.get_post(id)
-        return render_template("post/post.html", post=post)
-    else:
+@bp.route("/<int:post_id>")
+def post(post_id: int):
+    post = posts_db.get_post(post_id)
+    if post == None:
         abort(404, f"A post with id: '{post_id}' does not exsist")
+    else:
+        return render_template("post/post.html", post=post)
+
 
 @bp.route("/add", methods=["GET", "POST"])
 def add_post():
     if request.method == "POST":
         publised_at = int(time.time())
         author_id = request.form["author"]
-        header = request.form["header"]
+        title = request.form["title"]
         article = request.form["article"]
         visebility = request.form["visebility"]
-        if not header or not article:
-            return render_template("post/add.html", err="Header or Article missing")
+        if not title or not article:
+            return render_template("post/add.html", err="title or Article missing")
         else:
-            posts_db.add_post(author_id, publised_at, header, article, visebility)
+            posts_db.add_post(author_id, publised_at, title, article, visebility)
             return redirect("/", err=err)
     else:
         return render_template("post/add.html")
@@ -34,10 +35,10 @@ def add_post():
 def edit_post(post_id: int):
     if request.method == "POST":
         edited_at = int(time.time())
-        header = request.form["header"]
+        title = request.form["title"]
         article = request.form["article"]
         visebility = request.form["visebility"]
-        posts_db.edit_post(post_id, edited_at, header, article, visebility)
+        posts_db.edit_post(post_id, edited_at, title, article, visebility)
     else:
         post = posts_db.get_post(id)
         if post is None:
