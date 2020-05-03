5e7349602faddf1de6fcfced4c3658a80fa137c5
Marius <marius-sol@pm.me>
2020-05-03 22:35:32 +0200

    index route

--- app/views/index.py
+++ app/views/index.py
@@ -1,11 +1,21 @@
 from flask import Blueprint, render_template, request
 from werkzeug.exceptions import abort
 
-from app.db import DB as db
+from app.db import posts_db
 
 bp = Blueprint("index", __name__)
 
 @bp.route("/")
 def index():
-    posts = db.get_posts()
-    return render_template("index.html", posts=posts)
+    posts = posts_db.get_posts()
+    authors = posts_db.author_name()
+
+    author_dict = dict()
+    for author in authors:
+        author_dict[author[0]] = dict(author)
+
+    posts_dict = list()
+    for post in posts:
+        posts_dict.append(dict(post))
+
+    return render_template("index.html", posts=reversed(posts_dict), authors=author_dict)
