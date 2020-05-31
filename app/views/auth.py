eaa8df6ae164b73ab24afbe2d3613d19c685a62d
Marius <marius-sol@pm.me>
2020-06-01 00:49:05 +0200

    changed auth

--- app/views/auth.py
+++ app/views/auth.py
@@ -1,36 +1,56 @@
+import time
 import hashlib
+import re
 from functools import wraps
 
 from flask import Blueprint, redirect, render_template, request, url_for, session, flash, g
 
-from app.db import user
+from app.db import user_db
 
 bp = Blueprint("auth", __name__)
 
-@bp.route("/registrer", methods=["GET", "POST"])
-def registrer():
+
+@bp.before_request
+def load_user():
+    user_session = session.get("user_id")
+    if user_session is None:
+        g.user = None
+    else:
+        g.user = user_db.get_userID(user_session)
+
+
+@bp.route("/register", methods=["GET", "POST"])
+def register():
     if request.method == "POST":
         username = request.form["username"]
+        username = username.lower()
         password = request.form["password"]
 
         err = None
+
+        pattern = re.compile("[^a-z0-9.-_]")
+        if pattern.search(username):
+            err = "Username is not valid, Can only contain a-z, 0-9 and -_."
 
         if not username:
             err = "A username is required"
         elif not password:
             err = "A password is required"
         else:
-            if user.get_username(username):
+            if user_db.get_username(username):
                 err = "Username is already taken"
-
+    
         if err is None:
+            registered_at = int(time.time())
             hashed_password = hash_password(password)
-            user.add_user(username, hashed_password)
-            return redirect(url_for(auth.login))
+            user_db.add_user(username, hashed_password, registered_at)
+            flash(f"User: {username} have been succsefully made, you can now login", "success")
+            return redirect(url_for("auth.login"))
         else:
-            flash(err, error)
+            flash(err, "warning")
+            return redirect(url_for("auth.register"))
     else:
-        return render_template("auth/registrer.html")
+        return render_template("auth/register.html")
 
 
 
@@ -38,16 +58,19 @@ def registrer():
 def login():
     if request.method == "POST":
         username = request.form["username"]
+        username = username.lower()
         password = request.form["password"]
+
+        print(f"username={username}\nPassword={password}")
 
         err = None
 
-        user = user.get_username(username)
+        user = user_db.get_username(username)
 
         if user is None:
             err = "Wrong Username or Password"
 
-        if not check_hash(username, password):
+        elif check_hash(username, password) != True:
             err = "Wrong Username or Password"
 
         if err is None:
@@ -55,7 +78,8 @@ def login():
             session["user_id"] = user["user_id"]
             return redirect(url_for("index"))
         else:
-            flash(err, error)
+            flash(err, "warning")
+            return redirect(url_for("auth.login"))
     else:
         return render_template("auth/login.html")
 
@@ -70,17 +94,9 @@ def login_required(f):
         if "user_id" in session:
             return f(*args, **kwargs)
         else:
-            flash("You must be logged in to see this page")
+            flash("You must be logged in to see this page", "info")
             return redirect(url_for("auth.login"))
     return check_login_status
-
-@bp.before_request
-def load_user():
-    user_session = session.get("user_id")
-    if user_session is None:
-        g.user = None
-    else:
-        g.user = user.get_userID(user_session)
 
 def hash_password(password):
     password = password.encode("utf-8")
@@ -88,7 +104,7 @@ def hash_password(password):
     return pass_hash
 
 def check_hash(username, password):
-    user = user.get_username(username)
+    user = user_db.get_username(username)
     if user["password"] == hash_password(password):
         return True
     else:
