d0964c5e1ef4d9d7b408f6a5dc4037652df574eb
Marius <marius-sol@pm.me>
2020-06-01 00:43:27 +0200

    Adds blueprints to init func

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,16 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_mapping()
+
+    app.secret_key = "1"
 
     from app import db
     db.init_app(app)
+
+    from app.views import auth, index, posts
+    app.register_blueprint(auth.bp, url_prefix="/user")
+    app.register_blueprint(index.bp)
+    app.register_blueprint(posts.bp, url_prefix="/post")
 
     app.add_url_rule("/", endpoint="index")
     return app
